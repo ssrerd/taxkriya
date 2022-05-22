dbffb5d538fcd75eda4d83122b43129f53e7da4a
ssrerd <detroid675@gmail.com>
2022-05-22 10:54:48 +0530

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,8 +43,33 @@ export default {
       },
       {
         src:"https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js", integrity:"sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/", crossorigin:"anonymous", defer: true
-      }
-    ]
+      },
+      {
+    type: 'text/javascript',
+    hid: 'fb-customer-chat',
+    body: true,
+    innerHTML: `
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", YOUR_PAGE_ID);
+      chatbox.setAttribute("attribution", "biz_inbox");
+
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v11.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));`
+  }
+    ],
+    __dangerouslyDisableSanitizersByTagID: { 'fb-customer-chat': ['innerHTML'] },
   },
   /*
   ** Global CSS
