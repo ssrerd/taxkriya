60e59e899a0add8a2a764e737bf9ea3d0a4b31cf
ssrerd <detroid675@gmail.com>
2022-05-22 10:56:01 +0530

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,8 @@
     <Navbar></Navbar>
     <nuxt />
     <Testimonial></Testimonial>
+    <div id="fb-root"></div>
+    <div id="fb-customer-chat" class="fb-customerchat"></div>
     <Menufooter></Menufooter>
   </div>
 </template>
